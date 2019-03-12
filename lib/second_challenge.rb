561adf7841750e05e5d90f650764c401249d7795
Kyle Graden <kylegraden3@gmail.com>
2019-03-12 08:41:45 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,10 +1,16 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
+      :patriarch => "Lord Montague"
+      :matriarch => "Lady Montague"
+      :hero => "Romeo"
+      :hero_friends => ["Benvolio", "Mercutio"]
       }, 
     :capulet => {
-      #your key/value pairs here!
+      :patriarch => "Lord Capulet"
+      :matriarch => "Lady Capulet"
+      :hero => "Juliet"
+      :hero_friends => ["Steven", "Nurse"]
       }}
 
   
